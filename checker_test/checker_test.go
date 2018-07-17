a731d1964f197239e66e93a428ab29087b630763
afuset33 <anticapitalblindspot@live.jp>
2018-07-17 12:50:13 +0900

    ComboUpperLowerCaseのテストコードを追加

--- checker_test/checker_test.go
+++ checker_test/checker_test.go
@@ -118,3 +118,131 @@ func TestLengthCheck(t *testing.T) {
 		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual7), strconv.FormatBool(expected7))
 	}
 }
+
+func TestComboUpperLowerCase(t *testing.T) {
+	/*
+		小文字のみ
+	*/
+	// input
+	value1 := "abcdefg"
+	// expected
+	expected1 := false
+	// exercise
+	actual1 := checker.ComboUpperLowerCase(value1)
+	// verify
+	if actual1 != expected1 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual1), strconv.FormatBool(expected1))
+	}
+
+	/*
+		大文字のみ
+	*/
+	// input
+	value2 := "ABCDEFG"
+	// expected
+	expected2 := false
+	// exercise
+	actual2 := checker.ComboUpperLowerCase(value2)
+	// verify
+	if actual2 != expected2 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual2), strconv.FormatBool(expected2))
+	}
+
+	/*
+		大文字小文字混合
+	*/
+	// input
+	value3 := "abcdABCD"
+	// expected
+	expected3 := true
+	// exercise
+	actual3 := checker.ComboUpperLowerCase(value3)
+	// verify
+	if actual3 != expected3 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual3), strconv.FormatBool(expected3))
+	}
+
+	/*
+		全角英字小文字大文字混合
+	*/
+	// input
+	value4 := "ａｂｃｄＡＢＣＤ"
+	// expected
+	expected4 := false
+	// exercise
+	actual4 := checker.ComboUpperLowerCase(value4)
+	// verify
+	if actual4 != expected4 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual4), strconv.FormatBool(expected4))
+	}
+
+	/*
+		半角数字
+	*/
+	// input
+	value5 := "1234567890"
+	// expected
+	expected5 := false
+	// exercise
+	actual5 := checker.ComboUpperLowerCase(value5)
+	// verify
+	if actual5 != expected5 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual5), strconv.FormatBool(expected5))
+	}
+
+	/*
+		半角記号
+	*/
+	// input
+	value6 := "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+	// expected
+	expected6 := false
+	// exercise
+	actual6 := checker.ComboUpperLowerCase(value6)
+	// verify
+	if actual6 != expected6 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual6), strconv.FormatBool(expected6))
+	}
+
+	/*
+		半角英字小文字大文字、半角数字、半角記号混合
+	*/
+	// input
+	value7 := "abcABC01234!.:@;"
+	// expected
+	expected7 := true
+	// exercise
+	actual7 := checker.ComboUpperLowerCase(value7)
+	// verify
+	if actual7 != expected7 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual7), strconv.FormatBool(expected7))
+	}
+
+	/*
+		半角英字小文字大文字、全角英字混合
+	*/
+	// input
+	value8 := "abcABCａｂｃＡＢＣ"
+	// expected
+	expected8 := true
+	// exercise
+	actual8 := checker.ComboUpperLowerCase(value8)
+	// verify
+	if actual8 != expected8 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual8), strconv.FormatBool(expected8))
+	}
+
+	/*
+		未入力
+	*/
+	// input
+	value9 := ""
+	// expected
+	expected9 := false
+	// exercise
+	actual9 := checker.ComboUpperLowerCase(value9)
+	// verify
+	if actual9 != expected9 {
+		t.Errorf("\ngot %s\nexpected %s\n", strconv.FormatBool(actual9), strconv.FormatBool(expected9))
+	}
+}
